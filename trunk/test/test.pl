6f51cb5b01f14d1670bfdfaf829c97cb9b8420c5
simon <simon@b55edaec-ae9b-11dd-8748-9355f1163679>
1995-07-05 17:18:09 +0000

    Added header comment.
    Parse command-line arguments.

--- trunk/test/test.pl
+++ trunk/test/test.pl
@@ -1,11 +1,22 @@
 #!/logiciels/public/divers/bin/perl5
+# Minimal useful application of the SNMP package.
+# Author: Simon Leinen  <simon@lia.di.epfl.ch>
+# RCS $Header: /home/leinen/CVS/SNMP_Session/test/test.pl,v 1.8 1995-07-05 17:18:09 simon Exp $
+######################################################################
+# This application sends a get request for three fixed MIB-2 variable
+# instances (sysDescr.0, sysContact.0 and ipForwarding.0) to a given
+# host.  The hostname and community string can be given as
+# command-line arguments.
+######################################################################
 
 require 5;
 
 require 'SNMP_Session.pm';
 use BER;
 
-srand();
+$hostname = shift @ARGV || &usage;
+$community = shift @ARGV || 'public';
+&usage if $#ARGV >= 0;
 
 %ugly_oids = ( "sysDescr.0" => "1.3.6.1.2.1.1.1.0",
 		"sysContact.0" => "1.3.6.1.2.1.1.4.0",
@@ -16,10 +27,9 @@ foreach (keys %ugly_oids) {
     $pretty_oids{$ugly_oids{$_}} = $_;
 }
 
+srand();
 $session = SNMP_Session->open ('liasg7', 'public', 161);
-for ($i = 0; $i < 10; ++$i) {
-    snmp_get ($session, qw(sysDescr sysContact ipForwarding));
-}
+snmp_get ($session, qw(sysDescr sysContact ipForwarding));
 $session->close ();
 1;
 
@@ -27,7 +37,9 @@ sub snmp_get
 {
     my($session, @oids) = @_;
     my($response, $bindings, $binding, $value, $oid);
+
     grep ($_ = $ugly_oids{$_.".0"}, @oids);
+
     if ($session->get_request_response (@oids)) {
 	$response = $session->{pdu_buffer};
 	($bindings) = $session->decode_get_response ($response);
@@ -41,4 +53,9 @@ sub snmp_get
     } else {
 	warn "Response not received.\n";
     }
+}
+
+sub usage
+{
+    die "usage: $0 hostname [community]";
 }
