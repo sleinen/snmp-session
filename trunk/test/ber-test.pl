c5b9078f8fffb73d6b2e6c38d30d0236dc227c61
simon <simon@b55edaec-ae9b-11dd-8748-9355f1163679>
1997-02-01 16:07:42 +0000

    Use integer.
    Added several test cases.

--- trunk/test/ber-test.pl
+++ trunk/test/ber-test.pl
@@ -1,13 +1,16 @@
-#!/usr/local/bin/perl
-
+#!/usr/local/bin/perl -w
 ######################################################################
 ### Name:	  ber-test.pl
 ### Date Created: Sat Feb  1 16:09:46 1997
 ### Author:	  Simon Leinen  <simon@switch.ch>
-### RCS $Id: ber-test.pl,v 1.1 1997-02-01 15:46:57 simon Exp $
+### RCS $Id: ber-test.pl,v 1.2 1997-02-01 16:07:42 simon Exp $
+######################################################################
+### Regression Tests for BER encoding/decoding
 ######################################################################
 
 use BER;
+
+use integer;
 
 my $exitcode = 0;
 &regression_test;
@@ -20,14 +23,15 @@ sub regression_test
     &eq_test ('encode_string ("public")', "\x04\x06\x70\x75\x62\x6C\x69\x63");
     &eq_test ('encode_int (0x4aec3116)', "\x02\x04\x4A\xEC\x31\x16");
     &equal_test ('(&BER::decode_intlike ("\x02\x01\x01"))[0]', 1);
-    &equal_test ('(&BER::decode_intlike ("\x02\x02\x01\x01"))[0]', 257);
+    &equal_test ('(&BER::decode_intlike ("\x02\x01\xff"))[0]', -1);
+    &equal_test ('(&BER::decode_intlike ("\x02\x02\x01\x02"))[0]', 258);
     &equal_test ('(&BER::decode_intlike ("\x02\x02\xff\xff"))[0]', -1);
-    &equal_test ('(&BER::decode_unsignedlike ("\x02\x02\xff\xff"))[0]', 65535);
-    &equal_test ('(&BER::decode_intlike ("\x02\x03\xff\xff\xff"))[0]', -1);
-    &equal_test ('(&BER::decode_unsignedlike ("\x02\x03\xff\xff\xff"))[0]', 16777215);
-    &equal_test ('(&BER::decode_intlike ("\x02\x04\xff\xff\xff\xff"))[0]', -1);
-    &equal_test ('(&BER::decode_unsignedlike ("\x02\x04\xff\xff\xff\xff"))[0]', 4294967295);
-    &equal_test ('(&BER::decode_intlike ("\x02\x05\x00\xff\xff\xff\xff"))[0]', 4294967295);
+    &equal_test ('(&BER::decode_intlike ("\x02\x03\x00\xff\xfe"))[0]', 65534);
+    &equal_test ('(&BER::decode_intlike ("\x02\x03\xff\xff\xfd"))[0]', -3);
+    &equal_test ('(&BER::decode_intlike ("\x02\x04\x00\xff\xff\xfd"))[0]', 16777213);
+    &equal_test ('(&BER::decode_intlike ("\x02\x04\xff\xff\xff\xfc"))[0]', -4);
+    &equal_test ('(&BER::decode_intlike ("\x02\x05\x00\xff\xff\xff\xfc"))[0]', 4294967292);
+    &eq_test ('(&BER::decode_string ("\x04\x06public"))[0]', "public");
 }
 
 sub eq_test
