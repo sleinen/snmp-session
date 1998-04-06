7e84f2f68f3acb7ed8d2f6203192934cc0fae849
Simon Leinen <simon.leinen@gmail.com>
1998-04-06 11:32:19 +0000

    (map_table, map_table_start_end, index_compare, oid_diff): New subroutines.
    (index_compare, oid_diff): New exported names.

--- lib/SNMP_Session.pm
+++ lib/SNMP_Session.pm
@@ -31,11 +31,16 @@ use vars qw(@ISA $VERSION @EXPORT $errmsg $suppress_warnings);
 use Socket;
 use BER;
 
-$VERSION = '0.56';
+sub map_table ($$$);
+sub map_table_start_end ($$$$$);
+sub index_compare ($$);
+sub oid_diff ($$);
+
+$VERSION = '0.57';
 
 @ISA = qw(Exporter);
 
-@EXPORT = qw(errmsg suppress_warnings);
+@EXPORT = qw(errmsg suppress_warnings index_compare oid_diff);
 
 my $default_debug = 0;
 
@@ -231,6 +236,92 @@ sub ber_error ($ )
 
   $errmsg =~ s/^/  /mg;
   return $this->error ("$type:\n$errmsg");
+}
+
+sub map_table ($$$) {
+  my ($session, $columns, $mapfn) = @_;
+
+  return map_table_start_end ($session, $columns, $mapfn, "", undef);
+}
+
+sub map_table_start_end ($$$$$) {
+  my ($session, $columns, $mapfn, $start, $end) = @_;
+
+  my @encoded_oids;
+  my $base_index = $start;
+
+  do {
+    @encoded_oids = @{$columns};
+    @encoded_oids = grep ($_=encode_oid (@{$_},split '\.',$base_index),
+			  @encoded_oids);
+    if ($session->getnext_request_response (@encoded_oids)) {
+      my $response = $session->pdu_buffer;
+      my ($bindings) = $session->decode_get_response ($response);
+      my $smallest_index = undef;
+      my @collected_values = ();
+
+      my @bases = @{$columns};
+      while ($bindings ne '') {
+	my ($binding, $oid, $value);
+	my $base = shift @bases;
+	($binding, $bindings) = decode_sequence ($bindings);
+	($oid, $value) = decode_by_template ($binding, "%O%@");
+
+	my $out_index;
+
+	$out_index = &oid_diff ($base, $oid);
+	my $cmp = index_compare ($out_index,$smallest_index);
+	if ($cmp == -1) {
+	  $smallest_index = $out_index;
+	  grep ($_=undef, @collected_values);
+	  push @collected_values, pretty_print ($value);
+	} elsif ($cmp == 1) {
+	  push @collected_values, undef;
+	} else {
+	  push @collected_values, pretty_print ($value);
+	}
+      }
+      &$mapfn ($smallest_index, @collected_values)
+	if $smallest_index;
+      $base_index = $smallest_index;
+    } else {
+      die "SNMP error";
+    }
+  }
+  while ($base_index
+	&& (!defined $end
+	   || index_compare ($base_index, $end) < 0));
+}
+
+sub index_compare ($$) {
+  my ($i1, $i2) = @_;
+  if (!$i1) {
+    return !$i2 ? 0 : 1;
+  } elsif (!$i2) {
+    return -1;
+  } else {
+    my ($f1,$r1) = split('\.',$i1,2);
+    my ($f2,$r2) = split('\.',$i2,2);
+
+    if ($f1 < $f2) {
+      return -1;
+    } elsif ($f1 > $f2) {
+      return 1;
+    } else {
+      return index_compare ($r1,$r2);
+    }
+  }
+}
+
+sub oid_diff ($$) {
+  my($base, $full) = @_;
+  my $base_dotnot = join ('.',@{$base});
+  my $full_dotnot = BER::pretty_oid ($full);
+
+  return undef unless substr ($full_dotnot, 0, length $base_dotnot)
+    eq $base_dotnot
+      && substr ($full_dotnot, length $base_dotnot, 1) eq '.';
+  substr ($full_dotnot, length ($base_dotnot)+1);
 }
 
 sub version { $VERSION; }
