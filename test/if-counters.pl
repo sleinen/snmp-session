fb7468317ca7c792a5e67a42f1ee595178647739
Simon Leinen <simon.leinen@gmail.com>
1999-02-21 12:36:29 +0000

    Added periodic polling.

--- test/if-counters.pl
+++ test/if-counters.pl
@@ -1,13 +1,13 @@
 #!/usr/local/bin/perl -w
-#
-# Demonstration code for table walking
-#
-# This script should serve as an example of how to "correctly"
-# traverse the rows of a table.  This functionality is implemented in
-# the map_table() subroutine.  The example script displays a few
-# columns of the RFC 1213 interface table and Cisco's locIfTable.  The
-# tables share the same index, so they can be handled by a single
-# invocation of map_table().
+###
+### Demonstration code for table walking
+###
+### This script should serve as an example of how to "correctly"
+### traverse the rows of a table.  This functionality is implemented in
+### the map_table() subroutine.  The example script displays a few
+### columns of the RFC 1213 interface table and Cisco's locIfTable.  The
+### tables share the same index, so they can be handled by a single
+### invocation of map_table().
 
 require 5.003;
 
@@ -15,6 +15,7 @@ use strict;
 
 use BER;
 use SNMP_Session;
+use POSIX;			# for exact time
 
 my $host = shift @ARGV || die;
 my $community = shift @ARGV || die;
@@ -28,23 +29,53 @@ my $locIfInBitsSec = [1,3,6,1,4,1,9,2,2,1,1,6];
 my $locIfOutBitsSec = [1,3,6,1,4,1,9,2,2,1,1,8];
 my $locIfDescr = [1,3,6,1,4,1,9,2,2,1,1,28];
 
-sub out_interface {
-  my ($index, $descr, $in, $out, $inpkts, $outpkts, $comment) = @_;
+my $clock_ticks = POSIX::sysconf( &POSIX::_SC_CLK_TCK );
 
-  grep (defined $_ && ($_=pretty_print $_),
-	($descr, $in, $out, $inpkts, $outpkts, $comment));
-  printf "%2d  %-24s %10s %10s %10s %10s %s\n",
-  $index,
-  defined $descr ? $descr : '',
-  defined $in ? $in : '-',
-  defined $out ? $out : '-',
-  defined $inpkts ? $inpkts : '-',
-  defined $outpkts ? $outpkts : '-',
-  defined $comment ? $comment : '';
+my %old;
+my $desired_interval = 5.0;
+my $sleep_interval = $desired_interval + 0.0;
+my $interval;
+
+sub out_interface {
+    my ($index, $descr, $in, $out, $inpkts, $outpkts, $comment) = @_;
+    my ($clock) = POSIX::times();
+    grep (defined $_ && ($_=pretty_print $_),
+	  ($descr, $in, $out, $inpkts, $outpkts, $comment));
+    return unless defined $in && defined $out && defined $inpkts && defined $outpkts;
+    if (!defined $old{$index}) {
+	printf "%2d  %-24s %10s %10s %10s %10s %s\n",
+	$index,
+	defined $descr ? $descr : '',
+	defined $in ? $in : '-',
+	defined $out ? $out : '-',
+	defined $inpkts ? $inpkts : '-',
+	defined $outpkts ? $outpkts : '-',
+	defined $comment ? $comment : '';
+    } else {
+	$interval = ($clock-$old{$index}->{'clock'}) * 1.0 / $clock_ticks;
+	printf "%2d  %-24s %10.1f %10.1f %10.1f %10.1f %s\n",
+	$index,
+	defined $descr ? $descr : '',
+	defined $in ? ($in-$old{$index}->{'in'})*8/$interval : 0,
+	defined $out ? ($out-$old{$index}->{'out'})*8/$interval : 0,
+	defined $inpkts ? ($inpkts-$old{$index}->{'inpkts'})/$interval : 0,
+	defined $outpkts ? ($outpkts-$old{$index}->{'outpkts'})/$interval : 0,
+	defined $comment ? $comment : '';
+    }
+    $old{$index} = {'in' => $in,
+		    'out' => $out,
+		    'inpkts' => $inpkts,
+		    'outpkts' => $outpkts,
+		    'clock' => $clock};
 }
 
 my $session = SNMP_Session->open ($host, $community, 161)
   || die "Opening SNMP_Session";
-$session->map_table ([$ifDescr,$ifInOctets,$ifOutOctets,$ifInUcastPkts,$ifOutUcastPkts,$locIfDescr],
-		     \&out_interface);
+while (1) {
+    $session->map_table ([$ifDescr,$ifInOctets,$ifOutOctets,$ifInUcastPkts,$ifOutUcastPkts,$locIfDescr],
+			 \&out_interface);
+    $sleep_interval -= ($interval - $desired_interval)
+	if defined $interval;
+    select (undef, undef, undef, $sleep_interval);
+}
 1;
