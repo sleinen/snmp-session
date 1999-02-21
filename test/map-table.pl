e36c3bfc0242af2f04980ea6c5ae148b70c1fbac
Simon Leinen <simon.leinen@gmail.com>
1999-02-21 15:26:45 +0000

    Parse `-v 2c' arguments.

--- test/map-table.pl
+++ test/map-table.pl
@@ -16,8 +16,34 @@ use strict;
 use BER;
 use SNMP_Session;
 
-my $host = shift @ARGV || die;
-my $community = shift @ARGV || die;
+sub usage () {
+    die "Usage: $0 [-v (1|2c)] host community\n";
+}
+
+my $version = '1';
+
+while (defined $ARGV[0] && $ARGV[0] =~ /^-/) {
+    if ($ARGV[0] =~ /^-v/) {
+	if ($ARGV[0] eq '-v') {
+	    shift @ARGV;
+	    usage () unless defined $ARGV[0];
+	} else {
+	    $ARGV[0] = substr($ARGV[0], 2);
+	}
+	if ($ARGV[0] eq '1') {
+	    $version = '1';
+	} elsif ($ARGV[0] eq '2c') {
+	    $version = '2c';
+	} else {
+	    usage ();
+	}
+    } else {
+	usage ();
+    }
+    shift @ARGV;
+}
+my $host = shift @ARGV || usage ();
+my $community = shift @ARGV || usage ();
 
 my $ifDescr = [1,3,6,1,2,1,2,2,1,2];
 my $ifInOctets = [1,3,6,1,2,1,2,2,1,10];
@@ -39,7 +65,10 @@ sub out_interface {
   defined $comment ? $comment : '';
 }
 
-my $session = SNMP_Session->open ($host, $community, 161)
+my $session =
+    ($version eq '1' ? SNMPv1_Session->open ($host, $community, 161)
+     : $version eq '2c' ? SNMPv2c_Session->open ($host, $community, 161)
+     : die "Unknown SNMP version $version")
   || die "Opening SNMP_Session";
 $session->map_table ([$ifDescr,$locIfInBitsSec,$locIfOutBitsSec,$locIfDescr],
 		     \&out_interface);
