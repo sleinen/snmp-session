219dd06acf7f60a49bc4b63642ad3462f36c8c88
Simon Leinen <simon.leinen@gmail.com>
1999-02-24 17:12:37 +0000

    Handle `-h' option.
    Handle `-t secs' option.
    
    (usage): New subroutine.

--- test/if-counters.pl
+++ test/if-counters.pl
@@ -20,11 +20,13 @@ use Curses;
 
 my $version = '1';
 
+my $desired_interval = 5.0;
+
 while (defined $ARGV[0] && $ARGV[0] =~ /^-/) {
     if ($ARGV[0] =~ /^-v/) {
 	if ($ARGV[0] eq '-v') {
 	    shift @ARGV;
-	    usage () unless defined $ARGV[0];
+	    usage (1) unless defined $ARGV[0];
 	} else {
 	    $ARGV[0] = substr($ARGV[0], 2);
 	}
@@ -33,17 +35,31 @@ while (defined $ARGV[0] && $ARGV[0] =~ /^-/) {
 	} elsif ($ARGV[0] eq '2c') {
 	    $version = '2c';
 	} else {
-	    usage ();
+	    usage (1);
 	}
+    } elsif ($ARGV[0] =~ /^-t/) {
+	if ($ARGV[0] eq '-t') {
+	    shift @ARGV;
+	    usage (1) unless defined $ARGV[0];
+	} else {
+	    $ARGV[0] = substr($ARGV[0], 2);
+	}
+	if ($ARGV[0] =~ /^[0-9]+(\.[0-9]+)?$/) {
+	    $desired_interval = $ARGV[0];
+	} else {
+	    usage (1);
+	}
+    } elsif ($ARGV[0] eq '-h') {
+	usage (0);
+	exit 0;
     } else {
-	usage ();
+	usage (1);
     }
     shift @ARGV;
 }
-my $host = shift @ARGV || die;
-my $community = shift @ARGV || die;
-
-my $desired_interval = 5.0;
+my $host = shift @ARGV || usage (1);
+my $community = shift @ARGV || "public";
+usage (1) if $#ARGV >= $[;
 
 my $ifDescr = [1,3,6,1,2,1,2,2,1,2];
 my $ifAdminStatus = [1,3,6,1,2,1,2,2,1,7];
@@ -69,6 +85,8 @@ my $linecount;
 sub out_interface {
     my ($index, $descr, $admin, $oper, $in, $out, $crc, $comment) = @_;
     my ($clock) = POSIX::times();
+    my $alarm = 0;
+
     grep (defined $_ && ($_=pretty_print $_),
 	  ($descr, $admin, $oper, $in, $out, $crc, $comment));
     $win->clrtoeol ();
@@ -84,12 +102,16 @@ sub out_interface {
 			       defined $crc ? $crc : '-',
 			       defined $comment ? $comment : ''));
     } else {
-	$interval = ($clock-$old{$index}->{'clock'}) * 1.0 / $clock_ticks;
-	my $d_in = $in ? ($in-$old{$index}->{'in'})*8/$interval : 0;
-	my $d_out = $out ? ($out-$old{$index}->{'out'})*8/$interval : 0;
-	my $d_crc = $crc ? ($crc-$old{$index}->{'crc'})/$interval : 0;
-	my $alarm = $d_crc != 0;
-	print STDERR "\007" if $alarm;
+	my $old = $old{$index};
+
+	$interval = ($clock-$old->{'clock'}) * 1.0 / $clock_ticks;
+	my $d_in = $in ? ($in-$old->{'in'})*8/$interval : 0;
+	my $d_out = $out ? ($out-$old->{'out'})*8/$interval : 0;
+	my $d_crc = $crc ? ($crc-$old->{'crc'})/$interval : 0;
+	$alarm = ($d_crc != 0)
+	    || ($d_out > 0 && $d_in == 0);
+	print STDERR "\007" if $alarm && !$old->{'alarm'};
+	print STDERR "\007" if !$alarm && $old->{'alarm'};
 	$win->standout() if $alarm;
 	$win->addstr ($linecount, 0,
 		      sprintf ("%2d  %-24s %10.1f %10.1f %10.1f %s\n",
@@ -104,7 +126,8 @@ sub out_interface {
     $old{$index} = {'in' => $in,
 		    'out' => $out,
 		    'crc' => $crc,
-		    'clock' => $clock};
+		    'clock' => $clock,
+		    'alarm' => $alarm};
     ++$linecount;
     $win->refresh ();
 }
@@ -159,3 +182,25 @@ while (1) {
     select (undef, undef, undef, $sleep_interval);
 }
 1;
+
+sub usage ($) {
+    warn <<EOM;
+Usage: $0 [-t secs] [-v (1|2c)] switch [community]
+       $0 -h
+
+  -h           print this usage message and exit.
+
+  -t secs      specifies the sampling interval.  Defaults to 5 seconds.
+
+  -v version   can be used to select the SNMP version.  The default
+   	       is SNMPv1, which is what most devices support.  If your box
+   	       supports SNMPv2c, you should enable this by passing "-v 2c"
+   	       to the script.  SNMPv2c is much more efficient for walking
+   	       tables, which is what this tool does.
+
+  switch       hostname or IP address of an LS1010 switch
+
+  community    SNMP community string to use.  Defaults to "public".
+EOM
+    exit (1) if $_[0];
+}
