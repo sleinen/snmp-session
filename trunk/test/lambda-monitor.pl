ed83294b41324e63cd0370f4247339d062f8bed9
leinen <leinen@b55edaec-ae9b-11dd-8748-9355f1163679>
2001-11-22 21:43:33 +0000

    Added HTML output.

--- trunk/test/lambda-monitor.pl
+++ trunk/test/lambda-monitor.pl
@@ -7,6 +7,10 @@ use SNMP_util;
 
 sub show_light_trail (@);
 sub show_bitmap ($$);
+sub print_html_header ();
+sub print_html_trailer ();
+
+my $html_file = "/opt/www/htdocs/lan/switchlambda/status.html";
 
 snmpmapOID (qw(amplifierOperStatus 1.3.6.1.4.1.2522.1.5.1.1.0
 	       amplifierLastChange 1.3.6.1.4.1.2522.1.5.1.2.0
@@ -55,14 +59,27 @@ my @westbound_amps = qw(
 my @amps = (@eastbound_amps, @westbound_amps);
 
 for (;;) {
+    open (HTML, ">$html_file.new");
+    print_html_header ();
     my $localtime = localtime();
     print "",$localtime,"\n";
+
+    print HTML "<p> Last updated: $localtime </p>\n";
+
     print "\nEastbound:\n";
+    print HTML "<table width=\"100%\"><tr><td align=\"left\" valign=\"top\"><h2> Eastbound </h2>\n";
     show_light_trail (@eastbound_amps);
+
+    print HTML "</td><td align=\"right\" valign=\"top\"><h2> Westbound </h2>";
     print "\nWestbound:\n";
     show_light_trail (@westbound_amps);
+
+    print HTML "</td></tr></table>\n";
     print "-"x 75,"\n";
-    sleep (300);
+    print_html_trailer ();
+    close (HTML);
+    rename ($html_file.".new",$html_file);
+    sleep (292);
 }
 1;
 
@@ -70,6 +87,8 @@ sub show_light_trail (@) {
     my @amps = @_;
     printf "%-16s%-8s%-8s\n", "node", "  in pwr", " out pwr";
     printf "%-16s%-8s%-8s\n", "name", "   dbM", "   dbM";
+
+    print HTML "<table>\n <tr>\n  <th>Amplifier</th>\n  <th>Input<br>Power<br>(dbM)</th>\n  <th>Output<br>Power<br>(dbM)</th>\n </tr>\n";
     foreach my $amp (@amps) {
 	my ($community,$nodename) = split (/@/,$amp);
 	my ($amp_status,
@@ -83,7 +102,12 @@ sub show_light_trail (@) {
 	show_bitmap ($amp_status, 5),
 	pretty_power_status ($in_status),
 	pretty_power_status ($out_status);
+	my ($in_class, $out_class)
+	    = (class_for_status ($in_status),
+	       class_for_status ($out_status));
+	print HTML "<tr><td>$nodename</td><td class=\"$in_class\">$in</td><td class=\"$out_class\">$out</td></tr>\n";
     }
+    print HTML "</table>\n";
 }
 
 sub show_bitmap ($$) {
@@ -97,4 +121,43 @@ sub show_bitmap ($$) {
 
 sub pretty_power_status ($) {
     return (qw(???? ---- minr majr CRIT))[$_[0]];
+}
+
+sub class_for_status ($) {
+    return (qw(weird normal minor major critical))[$_[0]];
+}
+
+sub print_html_header () {
+    my $expires = time + 300;
+    my $expire_string = http_date_string ($expires);
+    print HTML <<EOM;
+<html><head>
+<title>SWITCHlambda Amplifier Status</title>
+<meta http-equiv="Refresh" content="300">
+<meta http-equiv="Expires" content="$expire_string">
+<style type="text/css">
+td.normal {color: #ffffff}
+td.minor {color: #ff8080}
+td.major {color: #ff4040}
+td.critical {color: #ff0000}
+</style>
+</head><body bgcolor="#000000" text="#ffffff">\n<h1>SWITCHlambda Amplifier Status</h1>
+EOM
+}
+
+sub print_html_trailer () {
+    print HTML "</body></html>\n";
+}
+
+sub http_date_string ($) {
+    my ($time) = @_;
+    my @gmtime = gmtime $time;
+    my ($wday) = (qw(Sun Mon Tue Wed Thu Fri Sat))[$gmtime[6]];
+    my ($month) = (qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec))[$gmtime[4]];
+    my ($mday,$year,$hour,$min,$sec) = @gmtime[3,5,2,1,0];
+    if ($mday<10) {$mday = "0$mday"};
+    if ($hour<10) {$hour = "0$hour"};
+    if ($min<10) {$min = "0$min";}
+    if ($sec<10) {$sec = "0$sec";}
+    return "$wday, $mday $month ".($year+1900)." $hour:$min:$sec GMT";
 }
