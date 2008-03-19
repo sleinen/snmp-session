8a3697fd3825f81d1479031f14be2837aa9cd34f
leinen <leinen@b55edaec-ae9b-11dd-8748-9355f1163679>
2008-03-19 21:40:22 +0000

    Improved initialization of the flags for non-blocking behavior in receive_response_3().  The __DIE__ and __WARN__ signal handlers should be bound to the defaults, because the caller might be binding those and get in our way.  Also, we only want to compute the flags once, on initialization.

--- trunk/lib/SNMP_Session.pm
+++ trunk/lib/SNMP_Session.pm
@@ -133,9 +133,16 @@ $SNMP_Session::recycle_socket = 0;
 ### but this function is only available in recent versions of Socket.pm.
 my $ipv6_addr_len;
 
+### Flags to be passed to recv() when non-blocking behavior is
+### desired.  On most POSIX-like systems this will be set to
+### MSG_DONTWAIT, on other systems we leave it at zero.
+###
+my $dont_wait_flags;
+
 BEGIN {
     $ipv6_addr_len = undef;
     $SNMP_Session::ipv6available = 0;
+    $dont_wait_flags = 0;
 
     if (eval {local $SIG{__DIE__};require Socket6;} &&
        eval {local $SIG{__DIE__};require IO::Socket::INET6; IO::Socket::INET6->VERSION("1.26");}) {
@@ -143,6 +150,7 @@ BEGIN {
 	$ipv6_addr_len = length(pack_sockaddr_in6(161, inet_pton(AF_INET6(), "::1")));
 	$SNMP_Session::ipv6available = 1;
     }
+    eval 'local $SIG{__DIE__};local $SIG{__WARN__};$dont_wait_flags = MSG_DONTWAIT();';
 }
 
 my $the_socket;
@@ -832,7 +840,7 @@ sub receive_response_3 {
     my ($this, $response_tag, $oids, $errorp, $dont_block_p) = @_;
     my ($remote_addr);
     my $flags = 0;
-    eval '$flags = MSG_DONTWAIT;' if defined $dont_block_p and $dont_block_p;
+    $flags = $dont_wait_flags if defined $dont_block_p and $dont_block_p;
     $remote_addr = recv ($this->sock,$this->{'pdu_buffer'},$this->max_pdu_len,$flags);
     return $this->error ("receiving response PDU: $!")
 	unless defined $remote_addr;
